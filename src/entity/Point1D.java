b796818ed324473f5dad386447f54ac6e3778b1c
Vasya Golovastov <VSGolovastov@gmail.com>
2021-11-01 21:00:35 +0400

    add Get set12020111

---
+++ src/entity/Point1D.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Point1D {
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public double length() {
+        return Math.abs(x);
+    }
+}
---
+++ src/entity/Point2D.java
@@ -0,0 +1,20 @@
+package entity;
+
+import java.lang.reflect.Member;
+
+public class Point2D extends Point1D{
+    private int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public double length() {
+        return Math.hypot(y, getX());
+    }
+}
