b9186fda2e3407a1a6bbbadd7f2765c4624c92cf
Vasya Golovastov <VSGolovastov@gmail.com>
2021-11-03 17:37:57 +0400

    TestOwerloadingConstructor 17370311

---
+++ src/entity/TestOwerloadingConstructor/Quest.java
@@ -0,0 +1,14 @@
+/*package entity.TestOwerloadingConstructor;
+
+public class Quest {
+    public Quest () {}
+    public Quest (int i) {
+        this (i, i);
+    }
+    public Quest (int i, int j) {
+        this ();
+    }
+    public static void main(String[] args) {
+    Quest quest = new Quest(2, 3);//1
+    }
+}*/
---
+++ src/entity/TestOwerloadingConstructor/Quest2.java
@@ -0,0 +1,14 @@
+package entity.TestOwerloadingConstructor;
+
+public class Quest2 {
+    Quest2 (int i) {
+        System.out.println("Constructor");
+    }
+    public static void main (String[] args) {
+        /*Quest2 quest = new Quest2();//1
+    }
+    *//*public Quest2() {
+    }*//*
+     {*/ new Quest2(1);
+    }
+}
