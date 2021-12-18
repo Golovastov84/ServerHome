05cbfc3defd0d1987dc63e682ca0ac6ba98ba930
Vasya Golovastov <vsgolovastov@gmail.com>
2021-12-18 14:59:38 +0400

    improvement of the RandomGame program 1459 181221

--- src/MyCod/Kickstart/RandomGame.java
+++ src/MyCod/Kickstart/RandomGame.java
@@ -3,6 +3,28 @@ package MyCod.Kickstart;
 import java.util.Random;
 import java.util.Scanner;
 
+/*class Typetester {
+int printType(byte attempt) {
+    return 1;
+}
+int printType(int attempt) {
+    return 2;
+}
+int printType(float attempt) {
+    return 3;
+}
+int printType(double attempt) {
+    return 4;
+}
+int printType(char attempt) {
+    return 5;
+}
+int printType(String attempt) {
+    return 6;
+}
+
+}*/
+
 public class RandomGame {
     public static void main(String[] args) {
         while (true) {
@@ -12,14 +34,23 @@ public class RandomGame {
             int b = new Scanner(System.in).nextInt();
         System.out.println("Задайте количество попыток");
             int n = new Scanner(System.in).nextInt();
-
+                    System.out.println("Угадайте целое число от " + a + " до " + b);
         int value = new Random().nextInt((b - a) + 1) + a;
-        System.out.println("Угадайте целое число от " + a + " до " + b);
             //System.out.println(value);
             int i;
             for (i = 1; i <= n; i++) {
-                int attempt = new Scanner(System.in).nextInt();
+                Scanner scanner = new Scanner(System.in);
+                int attempt;
+                //int attempt = new Scanner(System.in).nextInt();
                 //WRONG_ANSWER
+                do {
+                    System.out.println("WRONG_ANSWER_1");
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("WRONG_ANSWER");
+                        scanner.next();
+                    }
+                    attempt = scanner.nextInt();
+                } while (attempt > b || attempt < a);
                 if (attempt == value) {
                     System.out.println("CORRECT");
                     break;
