b5ce581b6f91f809393653e9f884e2cf1278753f
Vasya Golovastov <vsgolovastov@gmail.com>
2021-12-21 19:23:31 +0400

    RandomGame_improved_1922_211221

--- src/MyCod/Kickstart/RandomGame.java
+++ src/MyCod/Kickstart/RandomGame.java
@@ -4,37 +4,51 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RandomGame {
+    public static void separatorPrint() {
+        System.out.println();
+        System.out.println("==================");
+        System.out.println();
+    }
+
+    public static int testScan() {
+        Scanner scannerTest = new Scanner(System.in);
+        while (!scannerTest.hasNextInt()) {
+            System.out.println("WRONG_ANSWER");
+            scannerTest.next();
+        }
+        return scannerTest.nextInt();
+    }
+
     public static void main(String[] args) {
         while (true) {
-        System.out.println("Задайте нижнюю границу: ");
-            int a = new Scanner(System.in).nextInt();
-        System.out.println("Задайте верхнюю границу: ");
-            int b = new Scanner(System.in).nextInt();
-        System.out.println("Задайте количество попыток");
-            int n = new Scanner(System.in).nextInt();
-                    System.out.println("Угадайте целое число от " + a + " до " + b);
-        int value = new Random().nextInt((b - a) + 1) + a;
-            //System.out.println(value);
+            System.out.println("Задайте нижнюю границу: ");
+            int a = testScan();
+
+            System.out.println("Задайте верхнюю границу: ");
+            int b = testScan();
+
+            System.out.println("Задайте количество попыток");
+            int n = testScan();
+
+            System.out.println("Угадайте целое число от " + a + " до " + b);
+            int value = new Random().nextInt((b - a) + 1) + a;
             int i;
             for (i = 1; i <= n; i++) {
                 Scanner scanner = new Scanner(System.in);
                 int attempt;
-                //int attempt = new Scanner(System.in).nextInt();
-                //WRONG_ANSWER
-                AddScan addScan = new AddScan(1,a,b,false);
                 do {
                     while (!scanner.hasNextInt()) {
                         System.out.println("WRONG_ANSWER");
                         scanner.next();
                     }
                     attempt = scanner.nextInt();
-                    addScan.setForAttempt(attempt);
-                    if(addScan.isIntervalHit()) {
-                        System.out.println("WRONG_ANSWER_1");
+                    if (attempt > b || attempt < a) {
+                        System.out.println("WRONG_ANSWER");
                     }
-                } while (addScan.isIntervalHit());
+                } while (attempt > b || attempt < a);
                 if (attempt == value) {
                     System.out.println("CORRECT");
+                    separatorPrint();
                     break;
                 } else if (attempt > value) {
                     System.out.println("TOO_BIG");
@@ -44,6 +58,7 @@ public class RandomGame {
             }
             if (i > n) {
                 System.out.println("Количество попыток превысило лимит " + n);
+                separatorPrint();
             }
         }
     }
