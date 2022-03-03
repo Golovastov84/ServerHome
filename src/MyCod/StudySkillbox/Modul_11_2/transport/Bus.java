525fbc627daa02578213e14b088342bfad44c752
Vasya Golovastov <vsgolovastov@gmail.com>
2022-03-03 16:50:09 +0400

    03.03.22 copy 2 - 3 тур 16,50

--- src/MyCod/StudySkillbox/Modul_11_2/transport/Bus.java
+++ src/MyCod/StudySkillbox/Modul_11_2/transport/Bus.java
@@ -60,7 +60,7 @@ public class Bus {
                 ", consumptionRate=" + consumptionRate +
                 '}';
     }
-    // copy 1 - 3 тур
+    // copy 2 - 3 тур
 
     public void refuel(double tankRate) {
         double total = tankFullnessRate + tankRate;
