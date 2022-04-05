1ad81926c55faaf8728bbed93a1f445ff8c7d154
Vasya Golovastov <vsgolovastov@gmail.com>
2022-04-05 20:01:05 +0400

    Modul_15_5_Webinar_recursion_2.0

--- src/MyCod/StudySkillbox/Modul_15_5/Main.java
+++ src/MyCod/StudySkillbox/Modul_15_5/Main.java
@@ -1,29 +1,25 @@
 package MyCod.StudySkillbox.Modul_15_5;
 
 import java.io.File;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "B:\\Головастов\\мои фото";
         File file = new File(folderPath);
 
-        System.out.println(file.length());
+        System.out.println(getFolderSize(file));
 
-//        System.out.println(System.getProperties().get("user.dir"));
-//        Set keys = System.getProperties().keySet();
-//        for(Object key : keys) {
-//            System.out.println(key);
-//        }
     }
 
     public static long getFolderSize(File folder) {
-        if(folder.isFile()) {
+        if (folder.isFile()) {
             return folder.length();
         }
+        long sum = 0;
         File[] files = folder.listFiles();
-        for(File file : files) {
-
+        for (File file : files) {
+            sum += getFolderSize(file);
         }
+        return sum;
     }
 }
