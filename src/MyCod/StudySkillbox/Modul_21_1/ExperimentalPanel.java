5935f4e76ec7977e9a1fa6f893782e2714d3a6ff
Golovastov84 <VSGolovastov@gmail.com>
2022-10-25 15:10:33 +0400

    Modul_21_1_desktop

---
+++ src/MyCod/StudySkillbox/Modul_21_1/ExperimentalPanel.java
@@ -0,0 +1,11 @@
+package MyCod.StudySkillbox.Modul_21_1;
+
+import javax.swing.*;
+
+public class ExperimentalPanel {
+    private JPanel drawPanel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- src/MyCod/StudySkillbox/Modul_21_1/Main.java
+++ src/MyCod/StudySkillbox/Modul_21_1/Main.java
@@ -1,14 +1,18 @@
 package MyCod.StudySkillbox.Modul_21_1;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(600, 400);
+
+        /*
+//      задание формы вручную
         frame.setLayout(new FlowLayout());
-        frame.add(new JButton("Click Me :)"));
+        frame.add(new JButton("Click Me :)"));*/
+
+        frame.add(new MainForm().getMainPanel());
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
