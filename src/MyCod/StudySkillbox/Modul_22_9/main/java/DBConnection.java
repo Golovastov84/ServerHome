f8e7ea62df0261db3b965bce161d6961e4594ac1
Golovastov84 <VSGolovastov@gmail.com>
2022-11-11 08:49:13 +0400

    Modul_22_12_SQL_multiple_request_1.5

--- src/MyCod/StudySkillbox/Modul_22_9/main/java/DBConnection.java
+++ src/MyCod/StudySkillbox/Modul_22_9/main/java/DBConnection.java
@@ -6,7 +6,9 @@ public class DBConnection
 
     private static String dbName = "learn";
     private static String dbUser = "root";
-    private static String dbPass = "ya78yrc8n4w3984";
+    private static String dbPass = "v67676?ar2";
+
+    private static StringBuilder insertQuery = new StringBuilder();
 
     public static Connection getConnection()
     {
@@ -22,7 +24,8 @@ public class DBConnection
                         "name TINYTEXT NOT NULL, " +
                         "birthDate DATE NOT NULL, " +
                         "`count` INT NOT NULL, " +
-                        "PRIMARY KEY(id))");
+                        "PRIMARY KEY(id), " +
+                        "UNIQUE KEY name_date(name(50), birthDate))");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -30,23 +33,38 @@ public class DBConnection
         return connection;
     }
 
+    public static void executeMultiInsert() throws SQLException{
+        String sql = "INSERT INTO voter_count(name, birthDate, `count`)" +
+                "VALUES" + insertQuery.toString() +
+                "ON DUPLICATE KEY UPDATE `count`=`count` + 1";
+        DBConnection.getConnection().createStatement().execute(sql);
+    }
+
     public static void countVoter(String name, String birthDay) throws SQLException
     {
         birthDay = birthDay.replace('.', '-');
-        String sql = "SELECT id FROM voter_count WHERE birthDate='" + birthDay + "' AND name='" + name + "'";
-        ResultSet rs = DBConnection.getConnection().createStatement().executeQuery(sql);
-        if(!rs.next())
-        {
-            DBConnection.getConnection().createStatement()
-                    .execute("INSERT INTO voter_count(name, birthDate, `count`) VALUES('" +
-                            name + "', '" + birthDay + "', 1)");
-        }
-        else {
-            Integer id = rs.getInt("id");
-            DBConnection.getConnection().createStatement()
-                    .execute("UPDATE voter_count SET `count`=`count`+1 WHERE id=" + id);
-        }
-        rs.close();
+        boolean isStart = insertQuery.length() == 0;
+        insertQuery.append((isStart ? "" : ",") + "('" + name + "', '" + birthDay + "', 1)");
+
+//        String sql = "INSERT INTO voter_count(name, birthDate, `count`)" +
+//                "VALUES('" + name + "', '" + birthDay + "', 1) " +
+//                "ON DUPLICATE KEY UPDATE `count`=`count` + 1";
+//        DBConnection.getConnection().createStatement().execute(sql);
+
+//        String sql = "SELECT id FROM voter_count WHERE birthDate='" + birthDay + "' AND name='" + name + "'";
+//        ResultSet rs = DBConnection.getConnection().createStatement().executeQuery(sql);
+//        if(!rs.next())
+//        {
+//            DBConnection.getConnection().createStatement()
+//                    .execute("INSERT INTO voter_count(name, birthDate, `count`) VALUES('" +
+////                            name + "', '" + birthDay + "', 1)");
+//        }
+//        else {
+//            Integer id = rs.getInt("id");
+//            DBConnection.getConnection().createStatement()
+//                    .execute("UPDATE voter_count SET `count`=`count`+1 WHERE id=" + id);
+//        }
+//        rs.close();
     }
 
     public static void printVoterCounts() throws SQLException
--- src/MyCod/StudySkillbox/Modul_22_9/main/java/Loader.java
+++ src/MyCod/StudySkillbox/Modul_22_9/main/java/Loader.java
@@ -21,25 +21,31 @@ public class Loader
     public static void main(String[] args) throws Exception
     {
         String fileName = "res/data-1M.xml";
-
+        long startTimer = System.currentTimeMillis();
+        long usage = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         parseFile(fileName);
 
+        usage = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() - usage;
+        System.out.println("usage: " + usage + " B");
+        System.out.println((System.currentTimeMillis() - startTimer) + " ms");
         //Printing results
-        System.out.println("Voting station work times: ");
-        for(Integer votingStation : voteStationWorkTimes.keySet())
-        {
-            WorkTime workTime = voteStationWorkTimes.get(votingStation);
-            System.out.println("\t" + votingStation + " - " + workTime);
-        }
+        DBConnection.printVoterCounts();
 
-        System.out.println("Duplicated voters: ");
-        for(Voter voter : voterCounts.keySet())
-        {
-            Integer count = voterCounts.get(voter);
-            if(count > 1) {
-                System.out.println("\t" + voter + " - " + count);
-            }
-        }
+//        System.out.println("Voting station work times: ");
+//        for(Integer votingStation : voteStationWorkTimes.keySet())
+//        {
+//            WorkTime workTime = voteStationWorkTimes.get(votingStation);
+//            System.out.println("\t" + votingStation + " - " + workTime);
+//        }
+//
+//        System.out.println("Duplicated voters: ");
+//        for(Voter voter : voterCounts.keySet())
+//        {
+//            Integer count = voterCounts.get(voter);
+//            if(count > 1) {
+//                System.out.println("\t" + voter + " - " + count);
+//            }
+//        }
     }
 
     private static void parseFile(String fileName) throws Exception
@@ -49,7 +55,7 @@ public class Loader
         Document doc = db.parse(new File(fileName));
 
         findEqualVoters(doc);
-        fixWorkTimes(doc);
+//        fixWorkTimes(doc);
     }
 
     private static void findEqualVoters(Document doc) throws Exception
@@ -62,12 +68,16 @@ public class Loader
             NamedNodeMap attributes = node.getAttributes();
 
             String name = attributes.getNamedItem("name").getNodeValue();
-            Date birthDay = birthDayFormat.parse(attributes.getNamedItem("birthDay").getNodeValue());
+//            Date birthDay = birthDayFormat.parse(attributes.getNamedItem("birthDay").getNodeValue());
+            String birthDay = attributes.getNamedItem("birthDay").getNodeValue();
 
-            Voter voter = new Voter(name, birthDay);
-            Integer count = voterCounts.get(voter);
-            voterCounts.put(voter, count == null ? 1 : count + 1);
+            DBConnection.countVoter(name, birthDay);
+
+//            Voter voter = new Voter(name, birthDay);
+//            Integer count = voterCounts.get(voter);
+//            voterCounts.put(voter, count == null ? 1 : count + 1);
         }
+        DBConnection.executeMultiInsert();
     }
 
     private static void fixWorkTimes(Document doc) throws Exception
