3680578c52ba9a5ea89409e037a6945c8e71027b
Golovastov84 <VSGolovastov@gmail.com>
2024-02-11 16:29:40 +0400

    Clone_CHPU_11_02_2024

--- src/MyCod/CPU_IZBS/Main.java
+++ src/MyCod/CPU_IZBS/Main.java
@@ -13,11 +13,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+public static TreeSet<Integer> frameNumbersZ;
+public static List<String> lines;
+public static int primeNumLine;
+public static int secondNumLine;
     public static void main(String[] args) {
         String text = "data/fr3r0_GL10_PVH.nc";
         int i = 0;
-        int target_star = 0;
-        int target_end = 0;
 
         Scanner sc = new Scanner(System.in);
         // количество повторов по х
@@ -27,64 +29,70 @@ public class Main {
         // граница между заготовками
         int border = 0;
         try {
-            List<String> lines = Files.readAllLines(Paths.get(text));
-            List<String> partManufacturingCode = new ArrayList<>();
-            List<Integer> frameNumbers = new ArrayList<>();
-//            System.out.println(lines.get(0));
-//            lines.forEach(c -> System.out.println(c));
-            File file = new File("data/Example.nc");
-            file.createNewFile();
-            FileWriter writer = new FileWriter(file);
-
-            for ( String line : lines
-                 ) {
-                i++;
-                String regex = "G0[12]"; // регулярное выражение
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(line);
-                while (matcher.find()) { // возврящает true когда найдено соответствие
-                    frameNumbers.add(i);
-                }
-            }
-
-
-            // вывод граничных номеров линий кода формирования детали
-            System.out.println(frameNumbers.get(0)+" "+frameNumbers.get(frameNumbers.size()-1));
-
-            // сохранение кода изготовления детали
-            i=0;
-            for ( String line : lines
-            ) {
-                i++;
-                if(frameNumbers.get(0)<=i && frameNumbers.get(frameNumbers.size()-1)>=i) {
-                    partManufacturingCode.add(line);
-                    partManufacturingCode.add("\n");
-                }
-            }
-
-            //вывод кода формирования детали
-           // System.out.println(partManufacturingCode);
-
-            // определение габаритов детали по х
-            float sizeX = DeterminingDimensionsPart(partManufacturingCode, "X[-]?[0-9.]+");
-            System.out.println(sizeX);
-
-            // определение габаритов детали по y
-            float sizeY = DeterminingDimensionsPart(partManufacturingCode, "Y[-]?[0-9.]+");
-            System.out.println(sizeY);
-
             System.out.println("Введите количество деталей по х");
             quantityByX = sc.nextInt();
             System.out.println("Введите количество деталей по y");
             quantityByY = sc.nextInt();
             System.out.println("Введите границу между заготовками в мм");
             border = sc.nextInt();
-            //System.out.println(quantityByX+"  "+quantityByY+" "+border);
+            System.out.println("Введите номер линии начала рабочего кода");
+            primeNumLine = sc.nextInt();
+            System.out.println("Введите номер линии окончания рабочего кода");
+            secondNumLine = sc.nextInt();
             sc.close();
+            lines = Files.readAllLines(Paths.get(text));
+            List<String> partManufacturingCode = new ArrayList<>();
+            File file = new File("data/Example.nc");
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+
+            List<String> startlines = new ArrayList<>();
+            List<String> endlines = new ArrayList<>();
+            List<String> finallines = new ArrayList<>();
+            for (int j = 0; j < primeNumLine + 1; j++) {
+                startlines.add(lines.get(j));
+               startlines.add("\n");
+            }
+            for (int w = secondNumLine + 1; w < lines.size(); w++) {
+                endlines.add(lines.get(w));
+                endlines.add("\n");
+            }
+            // нахождение максимального подъёма по Z
+            frameNumbersZ = new TreeSet<>();
+            i = 0;
+            for ( String line : lines
+            ) {
+                i++;
+                String regexZ = "Z[-]?[0-9.]+"; // регулярное выражение
+                Pattern patternZ = Pattern.compile(regexZ);
+                Matcher matcherZ = patternZ.matcher(line);
+                while (matcherZ.find()) { // возврящает true когда найдено соответствие
+                    frameNumbersZ.add(i);
+                }
+            }
+            // сохранение кода изготовления детали
+            i=0;
+            for ( String line : lines
+            ) {
+                i++;
+                if(primeNumLine<=i && secondNumLine>=i) {
+                    partManufacturingCode.add(line);
+                    partManufacturingCode.add("\n");
+                }
+            }
+
+            // определение габаритов детали по х
+            float sizeX = DeterminingDimensionsPart(partManufacturingCode, "X[-]?[0-9.]+");
+            System.out.println("Ширина движения центра фрезы по X: " + sizeX);
+
+            // определение габаритов детали по y
+            float sizeY = DeterminingDimensionsPart(partManufacturingCode, "Y[-]?[0-9.]+");
+            System.out.println("Ширина движения центра фрезы по Y: " + sizeY);
+
+
 
             // формирование кода набора деталей по Х
             List<String> partManufacturingCodeX = new ArrayList<>();
-            List<String> partManufacturingCodeY = new ArrayList<>();
             List<String> partManufacturingCodeXY = new ArrayList<>();
             String regexForX = "X[-]?[0-9.]+";
             String regexForY = "Y[-]?[0-9.]+";
@@ -96,13 +104,20 @@ public class Main {
 
 
             // запись данных в файл
-            writer.write(String.valueOf(partManufacturingCodeXY));
+            finallines.addAll(startlines);
+            finallines.addAll(partManufacturingCodeXY);
+            finallines.addAll(endlines);
+            String finallineStr = "";
+            for (String finalline : finallines
+                 ) {
+                finallineStr = finallineStr.concat(finalline);
+            }
+            writer.write(finallineStr);
             writer.flush();
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
 
     }
     public static float DeterminingDimensionsPart(List<String> partManufacturingCode, String regexX){
@@ -124,12 +139,13 @@ public class Main {
     }
 
     public static List<String> duplicationAlongGivenAxis (int quantityBy, int borderP, float size, String a,
-                                                          String regexFor,
-     List<String> partManufacturingCodeP){
+                                                          String regexFor,List<String> partManufacturingCodeP)
+    {
         List<String> partManufacturingCode = new ArrayList<>();
         for (int j = 0; j < quantityBy; j++) {
             // рассчет сдвига
             float displacement = j * (borderP + size);
+            //
             for (String part : partManufacturingCodeP
             ) {
                 Pattern patternFor = Pattern.compile(regexFor);
@@ -144,10 +160,10 @@ public class Main {
                     float originalFloat = Float.valueOf(originalString);
                     String correctedString =
                             Float.toString((float) (Math.round(1000 * (originalFloat + displacement))) / 1000);
-                    String correctedPart = part.replaceAll(sourceString, a + correctedString);
-                    partManufacturingCode.add(correctedPart.concat("\n"));
+                    part = part.replaceAll(sourceString, a + correctedString);
                     // заданного интревала
                 }
+                partManufacturingCode.add(part);
             }
         }
         return partManufacturingCode;
